ea63ec02c75c2feb62fa8138f5aecce6bbed0cbb
Ihor <fesianov.i@gmail.com>
2017-04-19 17:55:16 +0300

    city page with ajax

--- Fesianov_Ihor/city_page/main/main.py
+++ Fesianov_Ihor/city_page/main/main.py
@@ -1,36 +1,39 @@
 __author__ = 'Ihor'
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, jsonify
 from Fesianov_Ihor.city_page.weather.weather_service import WeatherService
 from Fesianov_Ihor.city_page.pictures.pictures_service import PicturesService
 app = Flask(__name__)
 
 WEATHER_SERVICE = WeatherService()
+NUM_OF_PICTURES = 16
 
 
 def get_weather_and_coord(city_name):
-    if city_name:
-        weather = WEATHER_SERVICE.get_weather(city_name)
-        return weather.get_main_info_separated(), weather.coord()
-    return '', ''
+    weather = WEATHER_SERVICE.get_weather(city_name)
+    return weather.get_main_info_dict(), weather.coord()
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    city_name = request.form.get('city')
-    weather_info, coord = get_weather_and_coord(city_name)
-    num_of_pictures = 16
-    pictures_urls = PicturesService(city_name).get_pictures_url(num_of_pictures)
-    return render_template('index.html',
-                           weather_info=weather_info,
-                           pictures_urls=pictures_urls,
-                           coord=coord)
+    return render_template('index.html')
 
 
-# todo how to use static files in flask
-@app.route(r'/css/<path:path>')
-def send_css(path):
-    return send_from_directory('/templates', path)
+@app.route('/process', methods=['POST'])
+def process():
+    city_name = request.form.get('city_name')
+    if city_name:
+        weather_info, coord = get_weather_and_coord(city_name)
+        pictures_urls = PicturesService(city_name).get_pictures_url(NUM_OF_PICTURES)
+        return jsonify(
+            {
+                'weather_info': weather_info,
+                'pictures_urls': pictures_urls,
+                'coord': coord
+            }
+        )
+    return jsonify({'error': 'Missing data!'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
